5902c9486c86592366dda429682b6d0841c3f0fa
DeanDai <xiaogang1003@gmail.com>
2015-09-19 22:47:49 +0800

    Update Storage.js

--- Storage.js
+++ Storage.js
@@ -1,1 +1,28 @@
-/* Need add content */
+// HTML LocalStorage
+var u = this;
+u.store = function(key, data) {
+    var ls = window.localStorage;
+    if(!window.localStorage) {
+      console.log("Your browser is not support local storage!");
+    }
+    if (!arguments.length){
+      ls.clear();
+    } else if (arguments.length == 1) {
+      try {
+        data = JSON.parse(ls.getItem(key));
+      } catch (e) {
+        data = null;
+      }
+      return data;
+    } else if (arguments.length == 2) {
+      if (!data) {
+        ls.removeItem(key);
+      } else {
+        try {
+          ls.setItem(key, JSON.stringify(data));
+        } catch (e) {
+          console.log("Set value failed");
+        }
+      }
+    }
+}
